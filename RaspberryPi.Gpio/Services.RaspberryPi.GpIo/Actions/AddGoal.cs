48ab9381df45cbfa2e33be4265212da7cd7044f6
Alexander Mantzoukas <ama@trustpilot.com>
2014-07-17 11:12:42 +0200

    Add an action class that implements the "Add-Goal" concept

---
+++ RaspberryPi.Gpio/Services.RaspberryPi.GpIo/Actions/AddGoal.cs
@@ -0,0 +1,37 @@
+﻿namespace Services.RaspberryPi.GpIo.Actions
+{
+    using Chains;
+    using Chains.Play;
+
+    public sealed class AddGoal : ReproducibleWithData<AddGoalData>, IChainableAction<Gpio4X7SegmentLedContext, Gpio4X7SegmentLedContext>
+    {
+        public AddGoal(AddGoalData data)
+            : base(data)
+        {
+        }
+
+        public Gpio4X7SegmentLedContext Act(Gpio4X7SegmentLedContext context)
+        {
+            if (Data.ChannelActivated == 1)
+            {
+                context.WorkUnitContext.LogLine("GOAL -- RED");
+                context.RedScore++;
+            }
+            else if (Data.ChannelActivated == 2)
+            {
+                context.WorkUnitContext.LogLine("GOAL -- BLUE");
+                context.BlueScore++;
+            }
+
+            if (context.RedScore > 10 || context.BlueScore > 10)
+            {
+                // A win action must be invoked here, probably with some effects.
+
+                context.RedScore = 0;
+                context.BlueScore = 0;
+            }
+
+            return context;
+        }
+    }
+}
---
+++ RaspberryPi.Gpio/Services.RaspberryPi.GpIo/Actions/AddGoalData.cs
@@ -0,0 +1,19 @@
+﻿namespace Services.RaspberryPi.GpIo.Actions
+{
+    using Chains.Play;
+
+    public class AddGoalData : SerializableSpecification
+    {
+        public int ChannelActivated;
+
+        public int McuSpeed;
+
+        public override int DataStructureVersionNumber
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
